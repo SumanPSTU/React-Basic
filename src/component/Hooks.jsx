739d30dbf4a2874bae31f096d01d63a3d70623b3
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-04-04 01:53:08 +0600

    update Hooks file

--- src/component/Hooks.jsx
+++ src/component/Hooks.jsx
@@ -3,6 +3,9 @@ import { useRef } from "react";
 const Hooks =()=>{
     const ref = useRef();
     const img = useRef();
+    const att = useRef();
+    const fname = useRef()
+     const lname = useRef();
     const link = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9WQ2FExmFKVDSZZz-sodJFItx9acJ4kkBxw&s";
     const click=()=>{
         
@@ -13,6 +16,14 @@ const Hooks =()=>{
         ref.current.innerHTML="<h1>Hello sumon</h1>";
         img.current.src=link;
         img.current.style.borderRadius='20px';
+        att.current.setAttribute=('height','200px');
+        att.current.setAttribute=('weight','200px');
+        att.current.setAttribute=('background','blue');
+        const v1 = fname.current.value;
+        const v2 = lname.current.value;
+        alert("your name is  "+v1+" "+v2);
+        fname.current.value ="";
+        lname.current.value="";
     }
     return(
 
@@ -21,6 +32,9 @@ const Hooks =()=>{
             <button onClick={click}>Clicke me </button>
             <div ref={ref}></div>
             <div style={{margin:'20px',height:'200px'}}><img ref={img} alt="" /></div>
+            <div ref={att}></div>
+            <input type="text" ref={fname} />
+            <input type="text" ref={lname} />
         </div>
 
 
